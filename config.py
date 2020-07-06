d8ff3054fb95f44998837008baf8193c5148971d
Pariya P <pariya.poocharoen@campus.viu.edu>
2020-07-05 23:31:49 -0400

    upload ETL project

---
+++ config.py
@@ -0,0 +1,9 @@
+sf_username = 'pariya.poocharoen@campus.viu.edu'
+sf_password = '4658jK0L'
+sf_security_token = 'smhFFMh2GlOkyc3fpJ8MBst2r'
+
+remote_db_endpoint = 'pariya9889.cfy4r8hfc6lc.us-east-2.rds.amazonaws.com'
+remote_db_port = '3306'
+remote_db_user = 'admin'
+remote_db_pwd = '4658jK0L'
+remote_db_name = 'gwsis'
